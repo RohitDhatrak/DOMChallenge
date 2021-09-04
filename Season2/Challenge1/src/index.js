1031e28bbaefc73674dfe8a312ea8c5d6c91b26a
Rohit Dhatrak <57338980+rohit436@users.noreply.github.com>
2021-09-04 13:46:12 +0530

    first commit

---
+++ Season2/Challenge1/src/index.js
@@ -0,0 +1,40 @@
+const INPUT_TIME = 10; // in seconds
+
+const button = document.querySelector("#run-btn");
+const progress = document.querySelector("#progress");
+
+const FACTOR = 25;
+const INTERVAL_TIME = (INPUT_TIME * 1000) / 4;
+const OFFSET = 100;
+let currentWidth = 0;
+let clickCount = 0;
+let isOpInProgress;
+
+button.addEventListener("click", () => {
+    clickCount++;
+    button.innerText = "Run " + clickCount;
+    if (!isOpInProgress) {
+        runProgressBar();
+        isOpInProgress = true;
+    }
+});
+
+function runProgressBar() {
+    const intervalId = setInterval(() => {
+        progress.style.width = currentWidth + FACTOR + "%";
+        currentWidth += FACTOR;
+    }, INTERVAL_TIME - OFFSET);
+
+    setTimeout(() => {
+        clearInterval(intervalId);
+        progress.style.width = 0;
+        currentWidth = 0;
+        clickCount--;
+        isOpInProgress = false;
+        button.innerText = "Run " + clickCount;
+        if (clickCount && !isOpInProgress) {
+            runProgressBar();
+            isOpInProgress = true;
+        }
+    }, INPUT_TIME * 1000);
+}
